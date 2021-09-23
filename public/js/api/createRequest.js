dcf619c0a941227d9ed3354b84e12221e5983b06
Sergey Stepanov <ser1653285@yandex.ru>
2021-09-23 05:08:26 +0300

    added createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,25 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const responseXHR = new XMLHttpRequest();
+    let url = 'http://localhost:8000/' + options.url;
 
+    responseXHR.responseType = 'json';
+
+    responseXHR.onreadystatechange = function () {
+        if (responseXHR.readyState === 4) {
+            if (responseXHR.response['success']) {
+                options.callback(null, responseXHR.response);
+            } else {
+                options.callback(responseXHR.response['error'], null);
+            }
+        }
+    };
+
+    try {
+        responseXHR.open(options.method, url);
+        responseXHR.send(options.data);
+    } catch (err) {
+        options.callback(new Error(err.message), null);
+    }
 };
