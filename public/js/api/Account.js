58db38418f4e77693163ff586be4d44bc16ec6da
Sergey Stepanov <ser1653285@yandex.ru>
2021-09-23 05:10:21 +0300

    added Account

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,19 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
+    static URL = '/account';
 
-  }
+    /**
+     * Получает информацию о счёте
+     * */
+    static get(id = '', callback) {
+        createRequest({
+            url: `${this.URL}/${id}`,
+            method: 'GET',
+            responseType: 'json',
+            callback: (err, response) => {
+                callback(err, response);
+            },
+        });
+    }
 }
